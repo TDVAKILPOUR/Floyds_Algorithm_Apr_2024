0513bf5d3f25d40c611459eac5b200a681cad6cd
Soori <soori.vakilpour@hotmail.com>
2024-04-11 15:53:58 +0100

    Imperative Iterative Floyds Algorithm slightly amended using numpy and not itertools

---
+++ Floyds_Algorithm_Recursive_v1.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+# This is a code for a recursive floyd algorithm)
+
--- Impertive_Iterative_Floyds_Algorithm.py
+++ Impertive_Iterative_Floyds_Algorithm.py
@@ -8,10 +8,11 @@ import numpy as np
 
 INF = np.inf
 
-graph = [[0, 7, INF, 8],
-         [INF, 0, 5, INF],
-         [INF, INF, 0, 2],
-         [INF, INF, INF, 0]]
+graph = [[0, 5, INF, 10],
+             [INF, 0, 3, INF],
+             [INF, INF, 0,   1],
+             [INF, INF, INF, 0]
+             ]
 
 MAX_LENGTH = len(graph[0])
 
